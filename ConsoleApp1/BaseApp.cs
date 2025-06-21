ea19a2dfee364ebeb757ae70a2d9646e5024b3d5
PC <PC@DESKTOP-22UBD43>
2025-06-21 13:54:54 +0300

    saving changes

---
+++ ConsoleApp1/BaseApp.cs
@@ -0,0 +1,43 @@
+﻿namespace ConsoleApp1
+{
+    public class BaseApp
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Vvedite number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if(number > 7)
+            {
+                Console.WriteLine("Hello");
+            }
+
+            Console.Write("Vvedite name: ");
+            string name = Console.ReadLine();
+
+            if (name == "John") 
+            {
+                Console.WriteLine("Hello, John");
+            }
+            else
+            {
+                Console.WriteLine("There is no such name");
+            }
+
+            Console.Write("Vvedite array: ");
+            int[] numbers = Console.ReadLine().Split(',').Select(int.Parse).ToArray();
+            List<int> multipledNumbers = new List<int>();
+
+            foreach(int num in numbers)
+            {
+                var multipledNumber = num * num * num;
+                multipledNumbers.Add(multipledNumber);
+            }
+
+            foreach(int num in multipledNumbers)
+            {
+                Console.Write(string.Join(", ", num));
+            }
+        }
+    }
+}
